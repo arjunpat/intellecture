98146ef693918de1afd03ffd944991f107ff1cdf
Tony <32372239+TtonyxX@users.noreply.github.com>
2020-04-10 14:16:58 -0700

    firebase and landing page set up

--- teacher/src/App.vue
+++ teacher/src/App.vue
@@ -14,9 +14,9 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn v-if="!started" @click="$router.push({ path: '/new' })">Start Lecture</v-btn>
+      <v-btn v-if="!started && loggedIn" @click="$router.push({ path: '/new' })">Start Lecture</v-btn>
       <v-btn class="red" v-if="started" @click="$router.push({ path: '/dashboard' }); started = false;">End Lecture</v-btn>
-      <v-btn class="ml-1 deep-orange accent-2" v-if="!started">Sign out <img id="avt-img" class="ml-2" v-bind:src="imageurl" width="25px"></v-btn>
+      <v-btn class="ml-1 deep-orange accent-2" v-if="!started && loggedIn">Sign out <img id="avt-img" class="ml-2" v-bind:src="imageurl" width="25px"></v-btn>
       <div v-if="started" class="ml-3" style="background-color: #AED581; padding: 5px 8px; border-radius: 7px;">
         <span class="mr-1" style="font-size: 20px; font-family: 'Roboto'; font-weight: 500;">ROOM:</span> <span class="text--primary font-weight-black" style="background: #ddd; border-radius: 7px; padding: 2px 10px; font-size: 25px;">j3238</span>
       </div>
@@ -38,6 +38,7 @@ export default {
   data: function () {
     return {
       started: false,
+      loggedIn: false,
       imageurl: 'https://tonyxin-8bae2.firebaseapp.com/images/tonyxin2.png'
     }
   }
--- teacher/src/router/index.js
+++ teacher/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Landing from '@/views/Landing.vue'
 import Dashboard from '@/views/Dashboard.vue'
 import Lecture from '@/views/Lecture.vue'
 import New from '@/views/New.vue'
@@ -7,6 +8,11 @@ import New from '@/views/New.vue'
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'Landing',
+    component: Landing
+  },
   {
     path: '/dashboard',
     name: 'Dashboard',
