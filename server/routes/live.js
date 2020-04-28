ce274a5e21ef747db8fd1806362bbfeef7c97240
Arjun Patrawala <ajpat1234@gmail.com>
2020-04-27 23:03:31 -0700

    updates

--- server/routes/live.js
+++ server/routes/live.js
@@ -44,20 +44,32 @@ function handleUpgrade(req) {
 router.get('/teacher/:lecture_uid', mw.queryAuth, mw.auth, mw.websocket, async (req, res) => {
   let { lecture_uid } = req.params;
 
-  if (lectures[lecture_uid] || await db.lectures.getOwner(lecture_uid) !== req.uid)
-    return res.send(responses.error());
-  
   let socket = await handleUpgrade(req);
+
+  if (lectures[lecture_uid] || await db.lectures.getOwner(lecture_uid) !== req.uid) {
+    socket.json({
+      type: 'error',
+      error: 'permissions'
+    });
+    return socket.terminate();
+  }
+  
   lectures[lecture_uid] = new LectureManager(lecture_uid, db, socket);
 });
 
 router.get('/student/:lecture_uid', mw.queryAuth, mw.auth, mw.websocket, async (req, res) => {
   let { lecture_uid } = req.params;
   
-  if (!lectures[lecture_uid])
-    return res.send(responses.error());
-  
   let socket = await handleUpgrade(req);
+  
+  if (!lectures[lecture_uid]) {
+    socket.json({
+      type: 'error',
+      error: 'lecture_not_initialized'
+    });
+    return socket.terminate();
+  }
+  
   lectures[lecture_uid].addStudent(req.uid, socket);
 });
 
