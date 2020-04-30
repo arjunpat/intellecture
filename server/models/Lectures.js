66b485c35ec36f2b7b1481401b5230f2b7ef563b
Arjun Patrawala <ajpat1234@gmail.com>
2020-04-30 02:13:19 -0700

    oopie bug

--- server/models/Lectures.js
+++ server/models/Lectures.js
@@ -14,8 +14,24 @@ class Lectures {
   }
 
   getLecture(lecture_uid) {
+
     return this.mysql.query(
-      'SELECT a.uid, a.created_at, a.class_uid, a.name as lecture_name, a.start_time, a.end_time, b.owner_uid, b.name as class_name FROM lectures a LEFT JOIN classes b ON a.class_uid = b.uid;', [lecture_uid]
+      `SELECT
+        a.uid,
+        a.created_at,
+        a.class_uid,
+        a.name as lecture_name,
+        a.start_time,
+        a.end_time,
+        b.owner_uid,
+        b.name as class_name
+      FROM
+        (SELECT uid, created_at, class_uid, name, start_time, end_time FROM lectures WHERE uid = "ggspv") a
+      LEFT JOIN
+        classes b
+      ON
+        a.class_uid = b.uid`,
+      [lecture_uid]
     ).then(d => d[0]);
   }
 
