5359d28adb75cd50e56de8da1c5f3bd0e177c8c9
Jonathan Liu <liu.z.jonathan@gmail.com>
2020-05-11 23:30:08 -0700

    Fixed logo

--- student/src/components/ButtonWithImage.vue
+++ student/src/components/ButtonWithImage.vue
@@ -1,23 +1,27 @@
 <template>
   <v-btn :color="color" @click="onClick" :dark="dark">
-    <v-img 
-      v-if="!isAvatar"
-      :src="src"
-      contain
-      max-height="2em"
-      max-width="2em"
-      class="btn-img"
-      :class="dark ? '' : ''"
-      transition="scale-transition"
-    ></v-img>
-    <v-avatar
-      v-else
-      height="2em"
-      width="2em"
-    >
-      <img :src="src">
-    </v-avatar>
-    {{ text }}
+    <template v-if="showImage">
+      <v-img 
+        v-if="!isAvatar"
+        :src="src"
+        contain
+        max-height="2em"
+        max-width="2em"
+        height="2em"
+        width="2em"
+        class="btn-img mr-2 float-left"
+        :class="dark ? '' : ''"
+        transition="scale-transition"
+      ></v-img>
+      <v-avatar
+        v-else
+        height="2em"
+        width="2em"
+      >
+        <img :src="src">
+      </v-avatar>
+    </template>
+    <span class="btn-text">{{ text }}</span>
   </v-btn>
 </template>
 
@@ -37,6 +41,7 @@ export default {
     color: {type: String, default: ''},
     dark: {type: Boolean, default: false},
     isAvatar: {type: Boolean, default: false},
+    showImage: {type: Boolean, default: true},
   },
   methods: {
     onClick(e) {
