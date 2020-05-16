5c4d1d36dffb7ed088316db6952e3fa05785e286
Arjun Patrawala <ajpat1234@gmail.com>
2020-05-16 15:10:28 -0700

    auth issue

--- server/routes/analytics.js
+++ server/routes/analytics.js
@@ -7,10 +7,9 @@ const db = require('../models');
 
 async function lecturePerms(req, res, next) {
   let { lecture_uid } = req.params;
-  console.log(lecture_uid);
 
   let lecture = await db.lectures.getLecture(lecture_uid);
-  if (lecture.account_uid === req.uid)
+  if (lecture && lecture.account_uid === req.uid)
     return next();
   return res.send(responses.error('permissions'));
 }
@@ -21,7 +20,7 @@ router.get('/lecture/:lecture_uid/students', mw.auth, lecturePerms, async (req, 
   res.send(responses.success(await db.lectures.getStudents(lecture_uid)));
 });
 
-router.get('/lecture/:lecture_uid/question/:question_uid/upvotes', async (req, res) => {
+router.get('/lecture/:lecture_uid/question/:question_uid/upvotes', mw.auth, lecturePerms, async (req, res) => {
   let { question_uid } = req.params;
 
   res.send(responses.success(await db.lectureQUpvotes.getStudents(question_uid)));
